8b536b24c3658b6939567bcb847e6e8265c02a6c
Anton Gromov <seed14112@yandex.ru>
2024-08-06 13:28:56 +0300

    Создание админок для моделей

--- message/admin.py
+++ message/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
-# Register your models here.
+from message.models import Message, Clients, MailingList, Attempt
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    """
+    Админка модели Message
+    """
+    list_display = ('id', 'title_letter')
+    search_fields = ('title_letter',)
+
+
+@admin.register(Clients)
+class ClientsAdmin(admin.ModelAdmin):
+    """
+    Админка модели Clients
+    """
+    list_display = ('id', 'name', 'email')
+    search_fields = ('name', 'email')
+
+
+@admin.register(MailingList)
+class MailingListAdmin(admin.ModelAdmin):
+    """
+    Админка модели MailingList
+    """
+    list_display = ('id', 'message', 'periodicity', 'status')
+    list_filter = ('periodicity', 'status')
+    search_fields = ('message',)
+
+
+@admin.register(Attempt)
+class AttemptAdmin(admin.ModelAdmin):
+    """
+    Админка модели Attempt
+    """
+    list_display = ('mailing_list', 'status', 'mail_server_response')
+    list_filter = ('status',)
+    search_fields = ('mailing_list', 'mail_server_response')
+
